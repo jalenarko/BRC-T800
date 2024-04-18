abc5d96bf0ec9e943b73ef1fd3b343f64b5f2686
jalenarko <93200104+jalenarko@users.noreply.github.com>
2024-04-18 17:34:03 +0200

    Add files via upload

---
+++ start.js
@@ -0,0 +1,10 @@
+   // Add event listeners to the buttons
+document.addEventListener('DOMContentLoaded', function() {
+    const startButton = document.querySelector('.start-button');
+
+    startButton.addEventListener('click', function() {
+        window.location.href = "test.html";
+    });
+});
+
+
